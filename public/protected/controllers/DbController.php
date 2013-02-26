2d86472f698effd22fa2107456e51b04c1b92e77
rock <rock@88jianshen.com>
2013-02-26 17:38:30 +0800

    db_back

--- public/protected/controllers/DbController.php
+++ public/protected/controllers/DbController.php
@@ -9,13 +9,60 @@
  */
 class DbController extends  Controller
 {
-    private  $_data;
+
     public  function actionIndex(){
+       if(isset($_POST['backup'])){
+           $fpath =dirname(Yii::app()->basePath);
+           $datapath = $fpath.'/'.trim($_POST['backup']);
+              if(file_exists($datapath)) {
+                  $handle = fopen($datapath.'/TableStructure.sql','w+');
+                  fwrite($handle,$this->getTableValues());
+                  fclose($handle);
+              }else{
+                  echo 'none'.$datapath;exit;
+              }
 
-       $db = new CMysqlSchema(app()->db);
-       
-       $this->render('index',$this->_data);
+       }
+       $this->render('index');
     }
+    //数据库所有表结构备份
+    public  function getTableStructure(){
+        $string = '';
+        foreach($this->getTableArray() as $tab){
+            $arr = Yii::app()->db->createCommand('show CREATE TABLE '.$tab)->queryAll();;
+            $string .= "/*Table structure for {$tab} `adrot` */ \n\n\r";
+            $string .= 'DROP TABLE IF EXISTS `'.$tab.'`;';
+            $string .= "\n\n\r";
+            $string .= $arr[0]['Create Table'];
+            $string .= "\n\n\r";
+        }
+        return $string;
 
+    }
+   //数据库所有表的内容备份
+    public  function getTableValues(){
+        $str = '';
+        foreach($this->getTableArray() as $tab){
+            $arr = Yii::app()->db->createCommand()->select()->from($tab)->queryAll();
+            if(count($arr)){
+                foreach($arr as $row){
+                    $str .= 'INSERT INTO `'.$tab.'` values(';
+                    $str .= implode(',',array_map(array($this,'addSign'),array_values($row)));
+                    $str .=")<br/>";
+                }
+            }
+            $str .="<br/>";
+        }
+          return $str;
+    }
+    //给数组里面的值添加  ‘
+    public   function addSign($str){
+        return '\''.$str.'\'';
 
+    }
+    //所有表数组
+    public  function  getTableArray(){
+        $db =  new CMysqlSchema(Yii::app()->db);
+        return $db->tableNames;
+    }
 }
--- public/protected/views/db/index.php
+++ public/protected/views/db/index.php
@@ -1,3 +1,8 @@
-<p>备份数据库的目录</p>
-<?php  echo CHtml::textField('backup','backup')?>
-<?php  echo CHtml::button('备份数据库')?>
+<div style="width: 80%; margin: 0 auto; line-height: 30px;">
+    <p>备份数据库的目录:</p>
+    <form method="post">
+        <?php  echo CHtml::textField('backup','backup')?>
+        <?php  echo CHtml::submitButton('备份数据库')?>
+    </form>
+
+</div>
